ab385c98b034e3c6b67695f6ec0a1925dea25153
jeremydmiller <jeremydmiller@yahoo.com>
2013-06-12 09:00:49 -0500

    first couple tests and implementation for CodeFileTemplate

---
+++ src/FubuCsProjFile.Testing/Templating/CodeFileTemplateTester.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using FubuCore;
+using FubuCsProjFile.Templating;
+using NUnit.Framework;
+using FubuTestingSupport;
+using System.Linq;
+
+namespace FubuCsProjFile.Testing.Templating
+{
+    [TestFixture]
+    public class CodeFileTemplateTester
+    {
+        private CsProjFile theProject;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            new FileSystem().DeleteDirectory("Templated");
+            new FileSystem().CreateDirectory("Templated");
+
+            theProject = CsProjFile.CreateAtSolutionDirectory("TemplatedProject", "Templated");
+
+        }
+
+        [Test]
+        public void get_namespace_shallow()
+        {
+            CodeFileTemplate.GetNamespace("Foo", "Lib1").ShouldEqual("Lib1");
+            CodeFileTemplate.GetNamespace("Foo.cs", "Lib1").ShouldEqual("Lib1");
+            CodeFileTemplate.GetNamespace("Sub/Foo.cs", "Lib1").ShouldEqual("Lib1.Sub");
+            CodeFileTemplate.GetNamespace("Sub/Foo", "Lib1").ShouldEqual("Lib1.Sub");
+            CodeFileTemplate.GetNamespace("Sub/Other/Foo", "Lib1").ShouldEqual("Lib1.Sub.Other");
+            CodeFileTemplate.GetNamespace("Sub/Other/Foo.cs", "Lib1").ShouldEqual("Lib1.Sub.Other");
+        }
+
+        [Test]
+        public void add_simple_class_to_root_of_project()
+        {
+            CodeFileTemplate.Class("Foo").Attach(theProject);
+
+            var file = "Templated".AppendPath("TemplatedProject", "Foo.cs");
+            File.Exists(file).ShouldBeTrue();
+
+            new FileSystem().ReadStringFromFile(file).ShouldEqualWithLineEndings(@"
+namespace TemplatedProject
+{
+    public class Foo
+    {
+
+    }
+}
+".Trim());
+
+            theProject.All<CodeFile>().Any(x => x.Include == "Foo.cs")
+                .ShouldBeTrue();
+        }
+
+
+        [Test]
+        public void add_deeper_class_to_root_of_project()
+        {
+            CodeFileTemplate.Class("Bar/Doer").Attach(theProject);
+
+            var file = "Templated".AppendPath("TemplatedProject", "Bar", "Doer.cs");
+            File.Exists(file).ShouldBeTrue();
+
+            new FileSystem().ReadStringFromFile(file).ShouldEqualWithLineEndings(@"
+namespace TemplatedProject.Bar
+{
+    public class Doer
+    {
+
+    }
+}
+".Trim());
+
+            theProject.All<CodeFile>().Any(x => x.Include == "Bar/Doer.cs")
+                .ShouldBeTrue();
+        }
+
+
+    }
+}
---
+++ src/FubuCsProjFile/Templating/TemplatingClasses.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using FubuCore;
+using System.Linq;
+
+namespace FubuCsProjFile.Templating
+{
+    public interface IItemTemplate
+    {
+        void Attach(CsProjFile file);
+    }
+
+    public class CodeFileTemplate : IItemTemplate
+    {
+        public const string ASSEMBLY_NAME = "%ASSEMBLYNAME%";
+        public const string NAMESPACE = "%NAMESPACE%";
+        public const string CLASS = "%CLASS%";
+
+        private readonly string _relativePath;
+        private readonly string _rawText;
+
+        public static CodeFileTemplate Class(string relativePath)
+        {
+            var @class = Path.GetFileNameWithoutExtension(relativePath);
+
+            var rawText = Assembly.GetExecutingAssembly()
+                                  .GetManifestResourceStream(typeof (CodeFileTemplate), "Class.txt")
+                                  .ReadAllText()
+                                  .Replace(CLASS, @class);
+
+            if (Path.GetExtension(relativePath) != ".cs")
+            {
+                relativePath = relativePath + ".cs";
+            }
+
+            return new CodeFileTemplate(relativePath, rawText);
+        }
+
+        public CodeFileTemplate(string relativePath, string rawText)
+        {
+            if (Path.GetExtension(relativePath) != ".cs")
+            {
+                throw new ArgumentOutOfRangeException("relativePath", "Relative Path must have the .cs extension");
+            }
+
+            _relativePath = relativePath.Replace('\\', '/');
+
+            _rawText = rawText;
+        }
+
+        public string RelativePath
+        {
+            get { return _relativePath; }
+        }
+
+        public string RawText
+        {
+            get { return _rawText; }
+        }
+
+        public static string GetNamespace(string relativePath, string projectName)
+        {
+            return relativePath
+                .Split('/')
+                .Reverse()
+                .Skip(1)
+                .Union(new string[]{projectName})
+                .Reverse()
+                .Join(".");
+        }
+
+        public void Attach(CsProjFile file)
+        {
+            var @namespace = GetNamespace(_relativePath, file.ProjectName);
+
+            var filename = file.FileName.ParentDirectory().AppendPath(_relativePath);
+            if (!filename.EndsWith(".cs"))
+            {
+                filename = filename + ".cs";
+            }
+
+            var text = _rawText
+                .Replace(ASSEMBLY_NAME, file.ProjectName)
+                .Replace(NAMESPACE, @namespace);
+
+            new FileSystem().WriteStringToFile(filename, text);
+
+            file.Add<CodeFile>(_relativePath);
+        }
+    }
+}
