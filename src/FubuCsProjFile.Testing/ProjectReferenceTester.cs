d2faf1faae02aa628ac9decb3d37dbb126d80120
jeremydmiller <jeremydmiller@yahoo.com>
2013-06-11 20:52:11 -0500

    initial work on project reference

---
+++ src/FubuCsProjFile.Testing/ProjectReferenceTester.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuCsProjFile.Testing
+{
+    [TestFixture]
+    public class reading_a_project_reference_from_text
+    {
+        private ProjectReference theProject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            theProject = new ProjectReference("Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"FubuMVC.SlickGrid\", \"FubuMVC.SlickGrid\\FubuMVC.SlickGrid.csproj\", \"{A67A0CE1-E4C2-45FC-9019-829D434B2CC4}\"");
+        }
+
+        [Test]
+        public void the_reference_type()
+        {
+            theProject.ProjectType.ShouldEqual(Guid.Parse("FAE04EC0-301F-11D3-BF4B-00C04F79EFBC"));
+        }
+
+        [Test]
+        public void the_project_name()
+        {
+            theProject.ProjectName.ShouldEqual("FubuMVC.SlickGrid");
+        }
+
+        [Test]
+        public void the_relative_path()
+        {
+            theProject.RelativePath.ShouldEqual("FubuMVC.SlickGrid/FubuMVC.SlickGrid.csproj");
+        }
+    }
+}
---
+++ src/FubuCsProjFile/ProjectReference.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using FubuCore;
+using System.Linq;
+
+namespace FubuCsProjFile
+{
+    public class ProjectReference
+    {
+
+        public static readonly string ProjectLineTemplate = "Project(\"{0}\") = \"{1}\", \"{2}\", \"{{{3}}}\"";
+        private Guid _projectType;
+        private Guid _projectGuid;
+        private string _projectName;
+        private string _relativePath;
+
+        public ProjectReference(string text)
+        {
+            var parts = text.ToDelimitedArray('=');
+            _projectType = Guid.Parse(parts.First().TextBetweenSquiggles());
+            _projectGuid = Guid.Parse(parts.Last().TextBetweenSquiggles());
+
+            var secondParts = parts.Last().ToDelimitedArray();
+            _projectName = secondParts.First().TextBetweenQuotes();
+            _relativePath = secondParts.ElementAt(1).TextBetweenQuotes().Replace("\\", "/"); // Windows is forgiving
+        }
+
+        public Guid ProjectType
+        {
+            get { return _projectType; }
+        }
+
+        public string ProjectName
+        {
+            get { return _projectName; }
+        }
+
+        public string RelativePath
+        {
+            get { return _relativePath; }
+        }
+    }
+
+    public static class CsProjFileExtensions
+    {
+        public static string TextBetweenSquiggles(this string text)
+        {
+            var start = text.IndexOf("{");
+            var end = text.IndexOf("}");
+
+            return text.Substring(start + 1, end - start - 1);
+        }
+
+        public static string TextBetweenQuotes(this string text)
+        {
+            return text.Trim().TrimStart('"').TrimEnd('"');
+        }
+    }
+}
