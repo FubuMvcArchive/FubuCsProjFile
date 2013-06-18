a0abab6be06ff1970b10477c2ba00d0ff4990ba9
jeremydmiller <jeremydmiller@yahoo.com>
2013-06-18 08:09:00 -0500

    added the TextFile class for laters

---
+++ src/FubuCsProjFile.Testing/Templating/TextFileTester.cs
@@ -0,0 +1,35 @@
+﻿using FubuCsProjFile.Templating;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuCsProjFile.Testing.Templating
+{
+    [TestFixture]
+    public class TextFileTester
+    {
+        [Test]
+        public void read_contents()
+        {
+            TextFile.FileSystem.WriteStringToFile("foo.txt", "a bunch of text");
+
+            var file = new TextFile("foo.txt");
+            file.ReadAll().ShouldEqual("a bunch of text");
+        }
+
+        [Test]
+        public void read_lines()
+        {
+            TextFile.FileSystem.WriteStringToFile("bar.txt", @"
+a
+b
+c
+d
+e
+f
+".Trim());
+
+            var file = new TextFile("bar.txt");
+            file.ReadLines().ShouldHaveTheSameElementsAs("a", "b", "c", "d", "e", "f");
+        }
+    }
+}
---
+++ src/FubuCsProjFile/Templating/TextFile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using FubuCore;
+
+namespace FubuCsProjFile.Templating
+{
+    public class TextFile
+    {
+        public static readonly IFileSystem FileSystem = new FileSystem();
+
+        private readonly string _path;
+
+        public TextFile(string path)
+        {
+            _path = path;
+        }
+
+        public string Path
+        {
+            get { return _path; }
+        }
+
+        public string ReadAll()
+        {
+            return FileSystem.ReadStringFromFile(_path);
+        }
+
+        public IEnumerable<string> ReadLines()
+        {
+            return ReadAll().Trim().SplitOnNewLine();
+        }
+    }
+}
