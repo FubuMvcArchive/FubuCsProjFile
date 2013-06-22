119256ec1d1c53a618ffd65caec617f7ddc79916
jeremydmiller <jeremydmiller@yahoo.com>
2013-06-22 12:28:12 -0500

    moving template plan builder out

--- src/FubuCsProjFile/Templating/TemplateClasses.cs
+++ src/FubuCsProjFile/Templating/TemplateClasses.cs
@@ -1,102 +1,8 @@
 ﻿using System;
-using System.IO;
-using FubuCore;
 using System.Collections.Generic;
 
 namespace FubuCsProjFile.Templating
 {
-    
-
-    public class TemplatePlanBuilder
-    {
-        private readonly ITemplateLibrary _library;
-
-        public TemplatePlanBuilder(ITemplateLibrary library)
-        {
-            _library = library;
-        }
-
-
-        public static void ConfigureProjectTemplate(Template template, TemplatePlan plan)
-        {
-            new ProjectPlanner().CreatePlan(template.Path, plan);
-        }
-
-
-        // TODO -- do a bulk validation of TemplateRequest against the library 
-        public TemplatePlan BuildPlan(TemplateRequest request)
-        {
-            var plan = new TemplatePlan(request.RootDirectory);
-            
-            if (request.SolutionName.IsNotEmpty())
-            {
-                determineSolutionFileHandling(request, plan);
-            }
-
-            applySolutionTemplates(request, plan);
-            applyProjectTemplates(request, plan);
-
-//
-//            request.TestingProjects.Each(proj => {
-//                var testingProject = proj.OriginalProject + ".Testing";
-//                var projectPlan = new ProjectPlan(testingProject);
-//                plan.Add(projectPlan);
-//                plan.Add(new CopyProjectReferences(proj.OriginalProject, testingProject));
-//
-//                _library.ApplyAll(proj.Templates, plan, ConfigureProjectTemplate);
-//            });
-
-
-
-            return plan;
-        }
-
-        private void applyProjectTemplates(TemplateRequest request, TemplatePlan plan)
-        {
-            request.Projects.Each(proj => {
-                var projectPlan = new ProjectPlan(proj.Name);
-                plan.Add(projectPlan);
-
-                var planner = new ProjectPlanner();
-                if (proj.Template.IsNotEmpty())
-                {
-                    planner.CreatePlan(_library.Find(TemplateType.Project, proj.Template).Path, plan);
-                }
-
-                _library.Find(TemplateType.Alteration, proj.Alterations)
-                        .Each(template => { planner.CreatePlan(template.Path, plan); });
-            });
-        }
-
-        private void applySolutionTemplates(TemplateRequest request, TemplatePlan plan)
-        {
-            var planner = new SolutionPlanner();
-            _library.Find(TemplateType.Solution, request.Templates).Each(template =>
-            {
-                planner.CreatePlan(template.Path, plan);
-            });
-        }
-
-        private static void determineSolutionFileHandling(TemplateRequest request, TemplatePlan plan)
-        {
-            var sourceDirectory = plan.SourceDirectory;
-            var expectedFile = sourceDirectory.AppendPath(request.SolutionName);
-            if (Path.GetExtension(expectedFile) != ".sln")
-            {
-                expectedFile += ".sln";
-            }
-
-            if (File.Exists(expectedFile))
-            {
-                plan.Add(new ReadSolution(expectedFile));
-            }
-            else
-            {
-                plan.Add(new CreateSolution(request.SolutionName));
-            }
-        }
-    }
-
     public class CopyProjectReferences : ITemplateStep
     {
         public CopyProjectReferences(string originalProject, string testProject)
@@ -111,11 +17,6 @@ namespace FubuCsProjFile.Templating
         }
     }
 
-    public class SolutionRequest
-    {
-        public string Name { get; set; }
-        public IEnumerable<string> Templates { get; set; } // This needs to get a default value
-    }
 
     public class TestProjectRequest
     {
---
+++ src/FubuCsProjFile/Templating/TemplatePlanBuilder.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using FubuCore;
+
+namespace FubuCsProjFile.Templating
+{
+    public class TemplatePlanBuilder
+    {
+        private readonly ITemplateLibrary _library;
+
+        public TemplatePlanBuilder(ITemplateLibrary library)
+        {
+            _library = library;
+        }
+
+
+        public static void ConfigureProjectTemplate(Template template, TemplatePlan plan)
+        {
+            new ProjectPlanner().CreatePlan(template.Path, plan);
+        }
+
+
+        // TODO -- do a bulk validation of TemplateRequest against the library 
+        public TemplatePlan BuildPlan(TemplateRequest request)
+        {
+            var plan = new TemplatePlan(request.RootDirectory);
+            
+            if (request.SolutionName.IsNotEmpty())
+            {
+                determineSolutionFileHandling(request, plan);
+            }
+
+            applySolutionTemplates(request, plan);
+            applyProjectTemplates(request, plan);
+
+            return plan;
+        }
+
+        private void applyProjectTemplates(TemplateRequest request, TemplatePlan plan)
+        {
+            request.Projects.Each(proj => {
+                var projectPlan = new ProjectPlan(proj.Name);
+                plan.Add(projectPlan);
+
+                var planner = new ProjectPlanner();
+                if (FubuCore.StringExtensions.IsNotEmpty(proj.Template))
+                {
+                    planner.CreatePlan(_library.Find((TemplateType) TemplateType.Project, (string) proj.Template).Path, plan);
+                }
+
+                _library.Find((TemplateType) TemplateType.Alteration, (IEnumerable<string>) proj.Alterations)
+                        .Each(template => { planner.CreatePlan(template.Path, plan); });
+            });
+        }
+
+        private void applySolutionTemplates(TemplateRequest request, TemplatePlan plan)
+        {
+            var planner = new SolutionPlanner();
+            _library.Find(TemplateType.Solution, request.Templates).Each(template =>
+            {
+                planner.CreatePlan(template.Path, plan);
+            });
+        }
+
+        private static void determineSolutionFileHandling(TemplateRequest request, TemplatePlan plan)
+        {
+            var sourceDirectory = plan.SourceDirectory;
+            var expectedFile = sourceDirectory.AppendPath(request.SolutionName);
+            if (Path.GetExtension(expectedFile) != ".sln")
+            {
+                expectedFile += ".sln";
+            }
+
+            if (File.Exists(expectedFile))
+            {
+                plan.Add(new ReadSolution(expectedFile));
+            }
+            else
+            {
+                plan.Add(new CreateSolution(request.SolutionName));
+            }
+        }
+    }
+}
