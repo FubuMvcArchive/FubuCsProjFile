81a88704eb64380cf88d9d544a85b5096e457508
jeremydmiller <jeremydmiller@yahoo.com>
2013-06-27 08:54:56 -0500

    added the Substitutions class

---
+++ src/FubuCsProjFile.Testing/Templating/SubstitutionsTester.cs
@@ -0,0 +1,79 @@
+﻿using FubuCsProjFile.Templating;
+using NUnit.Framework;
+using FubuTestingSupport;
+
+namespace FubuCsProjFile.Testing.Templating
+{
+    [TestFixture]
+    public class SubstitutionsTester
+    {
+        [Test]
+        public void set_value()
+        {
+            var substitutions = new Substitutions();
+            substitutions.Set("key", "something");
+            substitutions.Set("two", "twenty");
+
+            substitutions.ValueFor("key").ShouldEqual("something");
+            substitutions.ValueFor("two").ShouldEqual("twenty");
+
+        }
+
+        [Test]
+        public void set_if_none_will_not_override_if_it_exists()
+        {
+            var substitutions = new Substitutions();
+            substitutions.Set("key", "something");
+            substitutions.SetIfNone("key", "different");
+
+            substitutions.ValueFor("key").ShouldEqual("something");
+        }
+
+        [Test]
+        public void set_if_none_will_write_if_no_previous_value()
+        {
+            var substitutions = new Substitutions();
+            substitutions.SetIfNone("key", "different");
+
+            substitutions.ValueFor("key").ShouldEqual("different");
+        }
+
+        [Test]
+        public void set_overrides()
+        {
+            var substitutions = new Substitutions();
+            substitutions.Set("key", "something");
+            substitutions.Set("key", "different");
+
+            substitutions.ValueFor("key").ShouldEqual("different");
+        }
+
+        [Test]
+        public void apply_substitutions()
+        {
+            var substitutions = new Substitutions();
+            substitutions.Set("%KEY%", "the key");
+            substitutions.Set("%NAME%", "George Michael");
+
+            var text = "%KEY% is %NAME%";
+            substitutions.ApplySubstitutions(text)
+                         .ShouldEqual("the key is George Michael");
+        }
+
+        [Test]
+        public void write_and_read()
+        {
+            var substitutions = new Substitutions();
+            substitutions.Set("key", "something");
+            substitutions.Set("two", "twenty");
+
+            substitutions.WriteTo("fubu.template.config");
+
+            var substitutions2 = new Substitutions();
+            substitutions2.ReadFrom("fubu.template.config");
+
+            substitutions2.ValueFor("key").ShouldEqual("something");
+            substitutions2.ValueFor("two").ShouldEqual("twenty");
+        }
+    }
+}
---
+++ src/FubuCsProjFile/Templating/Substitutions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using FubuCore;
+using FubuCore.Util;
+using System.Linq;
+
+namespace FubuCsProjFile.Templating
+{
+    public class Substitutions
+    {
+        private readonly Cache<string, string> _values = new Cache<string, string>();
+ 
+        public void SetIfNone(string key, string value)
+        {
+            _values.Fill(key, value);
+        }
+
+        public void Set(string key, string value)
+        {
+            _values[key] = value;
+        }
+
+        public string ValueFor(string key)
+        {
+            return _values[key];
+        }
+
+        public void ReadFrom(string file)
+        {
+            new FileSystem().ReadTextFile(file, line => {
+                if (line.IsEmpty()) return;
+
+                var parts = line.Split('=');
+                _values[parts.First()] = parts.Last();
+            });
+        }
+
+        public void WriteTo(string file)
+        {
+            new FileSystem()
+                .WriteToFlatFile(file, writer => _values.Each(writer.WriteProperty));
+        }
+
+        public string ApplySubstitutions(string rawText)
+        {
+            var builder = new StringBuilder(rawText);
+            _values.Each((key, value) => builder.Replace(key, value));
+
+            return builder.ToString();
+        }
+    }
+}
