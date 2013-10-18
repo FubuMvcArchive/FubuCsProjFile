10a61d94e260c1a68d19a48ce7f1948d662b762d
jeremydmiller <jeremydmiller@yahoo.com>
2013-10-18 07:26:03 -0500

    fleshed out the AssemblyReference object for some new things.  Closes GH-45

--- src/FubuCsProjFile.Testing/Templating/AssemblyReferenceTester.cs
+++ src/FubuCsProjFile.Testing/Templating/AssemblyReferenceTester.cs
@@ -1,4 +1,5 @@
-﻿using FubuCsProjFile.Templating;
+﻿using System.Security.Cryptography.X509Certificates;
+using FubuCsProjFile.Templating;
 using NUnit.Framework;
 using FubuCore;
 using FubuTestingSupport;
@@ -26,6 +27,178 @@ namespace FubuCsProjFile.Testing.Templating
 
             project.Find<AssemblyReference>("System.Configuration")
                    .ShouldNotBeNull();
+        }
+
+        [Test]
+        public void can_write_and_read_fusion_name()
+        {
+            var project = CsProjFile.CreateAtLocation("Foo.csproj", "Foo");
+            var assemblyReference = new AssemblyReference("Foo")
+            {
+                FusionName = "some fusion"
+            };
+
+            project.Add<AssemblyReference>(assemblyReference);
+
+            project.Save();
+
+            var project2 = CsProjFile.LoadFrom("Foo.csproj");
+
+            project2.Find<AssemblyReference>("Foo")
+                .FusionName.ShouldEqual("some fusion");
+        }
+
+        [Test]
+        public void can_write_and_read_Aliases()
+        {
+            var project = CsProjFile.CreateAtLocation("Foo.csproj", "Foo");
+            var assemblyReference = new AssemblyReference("Foo")
+            {
+                Aliases = "some alias"
+            };
+
+            project.Add<AssemblyReference>(assemblyReference);
+
+            project.Save();
+
+            var project2 = CsProjFile.LoadFrom("Foo.csproj");
+
+            project2.Find<AssemblyReference>("Foo")
+                .Aliases.ShouldEqual("some alias");
+        }
+
+        [Test]
+        public void can_write_and_read_display_name()
+        {
+            var project = CsProjFile.CreateAtLocation("Foo.csproj", "Foo");
+            var assemblyReference = new AssemblyReference("Foo")
+            {
+                DisplayName = "some name"
+            };
+
+            project.Add<AssemblyReference>(assemblyReference);
+
+            project.Save();
+
+            var project2 = CsProjFile.LoadFrom("Foo.csproj");
+
+            project2.Find<AssemblyReference>("Foo")
+                .DisplayName.ShouldEqual("some name");
+        }
+
+        [Test]
+        public void can_write_and_read_SpecificVersion_true()
+        {
+            var project = CsProjFile.CreateAtLocation("Foo.csproj", "Foo");
+            var assemblyReference = new AssemblyReference("Foo")
+            {
+                SpecificVersion = true
+            };
+
+            project.Add<AssemblyReference>(assemblyReference);
+
+            project.Save();
+
+            var project2 = CsProjFile.LoadFrom("Foo.csproj");
+
+            project2.Find<AssemblyReference>("Foo")
+                .SpecificVersion.Value.ShouldBeTrue();
+        }
+
+        [Test]
+        public void can_write_and_read_SpecificVersion_false()
+        {
+            var project = CsProjFile.CreateAtLocation("Foo.csproj", "Foo");
+            var assemblyReference = new AssemblyReference("Foo")
+            {
+                SpecificVersion = false
+            };
+
+            project.Add<AssemblyReference>(assemblyReference);
+
+            project.Save();
+
+            var project2 = CsProjFile.LoadFrom("Foo.csproj");
+
+            project2.Find<AssemblyReference>("Foo")
+                .SpecificVersion.Value.ShouldBeFalse();
+        }
+
+        [Test]
+        public void can_write_and_read_SpecificVersion_null()
+        {
+            var project = CsProjFile.CreateAtLocation("Foo.csproj", "Foo");
+            var assemblyReference = new AssemblyReference("Foo")
+            {
+                SpecificVersion = null
+            };
+
+            project.Add<AssemblyReference>(assemblyReference);
+
+            project.Save();
+
+            var project2 = CsProjFile.LoadFrom("Foo.csproj");
+
+            project2.Find<AssemblyReference>("Foo")
+                .SpecificVersion.ShouldBeNull();
+        }
+
+
+        [Test]
+        public void can_write_and_read_Private_true()
+        {
+            var project = CsProjFile.CreateAtLocation("Foo.csproj", "Foo");
+            var assemblyReference = new AssemblyReference("Foo")
+            {
+                Private = true
+            };
+
+            project.Add<AssemblyReference>(assemblyReference);
+
+            project.Save();
+
+            var project2 = CsProjFile.LoadFrom("Foo.csproj");
+
+            project2.Find<AssemblyReference>("Foo")
+                .Private.Value.ShouldBeTrue();
+        }
+
+        [Test]
+        public void can_write_and_read_Private_false()
+        {
+            var project = CsProjFile.CreateAtLocation("Foo.csproj", "Foo");
+            var assemblyReference = new AssemblyReference("Foo")
+            {
+                Private = false
+            };
+
+            project.Add<AssemblyReference>(assemblyReference);
+
+            project.Save();
+
+            var project2 = CsProjFile.LoadFrom("Foo.csproj");
+
+            project2.Find<AssemblyReference>("Foo")
+                .Private.Value.ShouldBeFalse();
+        }
+
+        [Test]
+        public void can_write_and_read_Private_null()
+        {
+            var project = CsProjFile.CreateAtLocation("Foo.csproj", "Foo");
+            var assemblyReference = new AssemblyReference("Foo")
+            {
+                Private = null
+            };
+
+            project.Add<AssemblyReference>(assemblyReference);
+
+            project.Save();
+
+            var project2 = CsProjFile.LoadFrom("Foo.csproj");
+
+            project2.Find<AssemblyReference>("Foo")
+                .Private.ShouldBeNull();
         }
     }
 }
--- src/FubuCsProjFile/AssemblyReference.cs
+++ src/FubuCsProjFile/AssemblyReference.cs
@@ -22,6 +22,11 @@ namespace FubuCsProjFile
         }
 
         public string HintPath { get; set; }
+        public string FusionName { get; set; }
+        public string DisplayName { get; set; }
+        public bool? SpecificVersion { get; set; }
+        public bool? Private { get; set; }
+        public string Aliases { get; set; }
 
         internal override MSBuildItem Configure(MSBuildItemGroup @group)
         {
@@ -31,8 +36,45 @@ namespace FubuCsProjFile
                 item.SetMetadata(HintPathAtt, HintPath);
             }
 
+            if (FusionName.IsNotEmpty())
+            {
+                item.SetMetadata("FusionName", FusionName);
+            }
+
+            if (Aliases.IsNotEmpty())
+            {
+                item.SetMetadata("Aliases", Aliases);
+            }
+
+            if (DisplayName.IsNotEmpty())
+            {
+                item.SetMetadata("Name", DisplayName);
+            }
+
+            if (SpecificVersion.HasValue)
+            {
+                item.SetMetadata("SpecificVersion", SpecificVersion.Value.ToString().ToLower());
+            }
+
+            if (Private.HasValue)
+            {
+                item.SetMetadata("Private", Private.Value.ToString().ToLower());
+            }
+
             return item;
         }
+
+        /*
+
+
+Aliases
+
+Optional string. Any aliases for the reference.
+
+Private
+
+Optional boolean. Specifies whether the reference should be copied to the output folder. This attribute matches the Copy Local property of the reference that's in the Visual Studio IDE.
+         */
 
         internal override void Read(MSBuildItem item)
         {
@@ -40,6 +82,19 @@ namespace FubuCsProjFile
 
 
             HintPath = item.HasMetadata(HintPathAtt) ? item.GetMetadata(HintPathAtt) : null;
+            FusionName = item.HasMetadata("FusionName") ? item.GetMetadata("FusionName") : null;
+            Aliases = item.HasMetadata("Aliases") ? item.GetMetadata("Aliases") : null;
+            DisplayName = item.HasMetadata("Name") ? item.GetMetadata("Name") : null;
+
+            if (item.HasMetadata("SpecificVersion"))
+            {
+                SpecificVersion = bool.Parse(item.GetMetadata("SpecificVersion"));
+            }
+
+            if (item.HasMetadata("Private"))
+            {
+                Private = bool.Parse(item.GetMetadata("Private"));
+            }
         }
     }
 }
