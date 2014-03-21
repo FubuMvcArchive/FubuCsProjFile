02572e6b298984a85e18b17ee754e1e92192fa45
Matthew Smith <mtscout6@gmail.com>
2014-03-21 13:07:18 -0600

    Added test for SolutionFolderSectionReader #84

---
+++ src/FubuCsProjFile.Testing/SolutionFile/SolutionItems/SolutionFolderSectionReaderTester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FubuCsProjFile.SolutionFile.SolutionItems;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuCsProjFile.Testing.SolutionFile.SolutionItems
+{
+    public class SolutionFolderSectionReaderTester
+    {
+        [Test]
+        public void read_simply_adds_line_to_raw_lines()
+        {
+            var folder = new SolutionFolder(Guid.NewGuid(), "projname", "relpath");
+            var reader = new SolutionFolderSectionReader(folder);
+
+            folder.RawLines.ShouldHaveTheSameElementsAs();
+            reader.Read("something");
+            folder.RawLines.ShouldHaveTheSameElementsAs("something");
+            reader.Read("something new");
+            folder.RawLines.ShouldHaveTheSameElementsAs("something", "something new");
+        }
+    }
+}
